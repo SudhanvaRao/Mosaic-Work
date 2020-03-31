9e1a627ce6d2aa3e1058d15bf435225e4942a93c
sudu521 <59615425+SudhanvaRao@users.noreply.github.com>
2020-03-31 16:53:25 +0530

    Update letter_detection.py
    with some minor changes

--- letter_detection.py
+++ letter_detection.py
@@ -2,56 +2,63 @@ import tensorflow as tf
 from tensorflow import keras
 import string
 import cv2
-immport random
-import os
+import random
+import gc
+import numpy as np
 
-PATH='characters'
-all_symbols=string.ascii_uppercase + string.ascii_lowercase +'0123456789' + '^%/*+-'
+PATH='/kaggle/input/letterzs/image'
+all_symbols=string.ascii_uppercase + string.ascii_lowercase +'0123456789'
+index_symbols=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','aa','bb','cc','dd','ee','ff','gg','hh','ii','jj','kk','ll','mm','nn','oo','pp','qq','rr','ss','tt','uu','vv','ww','xx','yy','zz','0','1','2','3','4','5','6','7','8','9']
 
 traindata=[]
-for char in all_symbols:
-	path=os.path.join(PATH,char)
-	for img in os.lisdir(path):
-		symb_index=all_symbol.index(char)
-		img_array=cv2.immread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
-		image_array=cv2.resize(img_array,(100,100))
-		traindata.append([image_array,symb_index])
+for char in index_symbols:
+    path=os.path.join(PATH,char)
+    for img in os.listdir(path):
+        symb_index=index_symbols.index(char)
+        img_array=cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+        print(os.path.join(path,img))
+        image_array=cv2.resize(img_array,(100,100))
+        traindata.append([image_array,symb_index])
 random.shuffle(traindata)
-Xfull=[]
-yfull=[]
 
-for freatures,lables in traindata:
-	Xfull.append(freatues)
-	yfull.append(labels)
-
-X=np.array(Xfull,dtype='float64')
-y=np.array(yfull,dtype='float64')
+traindata=[]
+for char in index_symbols:
+    path=os.path.join(PATH,char)
+    for img in os.listdir(path):
+        symb_index=index_symbols.index(char)
+        img_array=cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+        print(os.path.join(path,img))
+        image_array=cv2.resize(img_array,(100,100))
+        traindata.append([image_array,symb_index])
+random.shuffle(traindata)
 
 def model():
-	inputs = keras.Input(shape=(100,100,1))
-	x = keras.layers.Conv2D(200, (3,3),activation='relu')(inputs)
-	x_shortcut = x
-	x = keras.layers.MaxPooling2D((2,2))(x)
-	x = keras.layers.Add()([x,x_shortcut])
-	x = keras.layers.Conv2D(500, (3,3),activation='relu')(x)
-	x_shortcut = x
-	x = keras.layers.MaxPooling2D((2,2) , padding='same')(x)
-	x = keras.layers.Add()([x,x_shortcut])
-	x = keras.layers.Conv2D(500, (3,3),activation='relu')(x)
-	x_shortcut = x
-	x = keras.layers.MaxPooling2D((2,2))(x)
-	x = keras.layers.Add()([x,x_shortcut])
-	x = keras.layers.BatchNormalization()(x)
-	out_flat= keras.layers.Flatten()(x)
-	dense_1 = keras.layers.Dense(64 , activation='relu')(out_flat)
-	out_1 = keras.layers.Dense(len_symbols , activation='sigmoid')(dense_1)
-	model_out = keras.Model(inputs=inputs , outputs=out_1)
-	model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+    inputs = keras.Input(shape=(100,100,1))
+    x = keras.layers.Conv2D(200, (3,3),activation='relu',padding ='same')(inputs)
+    x_shortcut = x
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Add()([x,x_shortcut])
+    x = keras.layers.Conv2D(500, (3,3),activation='relu',padding='same')(x)
+    x_shortcut = x
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Add()([x,x_shortcut])
+    x = keras.layers.Conv2D(500, (3,3),activation='relu',padding='same')(x)
+    x_shortcut = x
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Add()([x,x_shortcut])
+    x = keras.layers.BatchNormalization()(x)
+    out_flat= keras.layers.Flatten()(x)
+    dense_1 = keras.layers.Dense(64 , activation='relu')(out_flat)
+    out_1 = keras.layers.Dense(len(all_symbols) , activation='relu')(dense_1)
+    model_out = keras.Model(inputs=inputs , outputs=out_1)
+    model_out.compile(loss='sparse_categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
 
-	return model_out
+    return model_out
 
 model1=model()
-model1.fit(X,y)
+model1.fit(X,y,validation_split=0.33,epochs=50)
 model.save('m1.h5')
-#newmodel=keras.models.load_model('m1.h5')
-#prediction=newmodel.predict(Xpredict)
+
+model2=model()
+model2.fit(X,y,validation_split==0.33,epoch=50)
+model.save('m2.h5')
